c11a91045a8ae459ad8a1d76a09807d304a41204
TheSamuraiBr <mmsnborges@gmail.com>
2023-06-14 15:31:14 -0300

    End

--- core/utils.py
+++ core/utils.py
@@ -1,28 +1,28 @@
 import requests
 from random import randint
 from django.conf import settings
-from django.contrib.gis.geoip2 import GeoIp2
+from django.contrib.gis.geoip2 import GeoIP2
 from django.contrib.gis.geoip2 import geoip2
 
 YELP_SEARCH_ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 
 def yelp_search(keyword=None, location=None):
-    headers("Authorization": "Bearer " + settings.YELP_API_KEY)
+    headers = {"Authorization": "Bearer " + settings.YELP_API_KEY}
 
     if keyword and location:
         params = {'term': keyword, 'location': location}
     else:
         params = {'term': 'Pizzaria', 'location': 'Porto Velho'}
 
-    r = request.get(YELP_SEARCH_ENDPOINT, headers=headers, params=params)
+    r = requests.get(YELP_SEARCH_ENDPOINT, headers=headers, params=params)
     return r.json()
 
 def get_client_data():
-    g = GeoIp2()
+    g = GeoIP2()
     ip = get_random_ip()
     try:
         return g.city(ip)
-    except geoip2.errors.AdrressNotFoundError:
+    except geoip2.errors.AddressNotFoundError:
         return None
 
 def get_random_ip():
